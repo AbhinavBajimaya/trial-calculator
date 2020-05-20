6578608eb183bd4c7dc4df2427604523f828f5c3
N-93 <63697021+N-93@users.noreply.github.com>
2020-05-21 00:14:34 +0545

    multiply function
    modified

---
+++ NK.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void multi(int,int);
+int main()
+{
+int a=2,b=5;
+//cout<<"this is the outout\n";
+multi(a,b);
+return 0;
+}
+void multi(int a,int b)
+{
+cout<<a*b;
+
+}
